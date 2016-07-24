e22bb3427de97d42da3bb211421d27f5c689accd
Danang <8danang8@gmail.com>
2016-07-24 21:20:34 +0700

    Banyak Error

---
+++ app/src/main/java/com/eightstudio/danboru/cinema123/splash/splashscreen.java
@@ -0,0 +1,46 @@
+package com.eightstudio.danboru.cinema123.splash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.eightstudio.danboru.cinema123.MainActivity;
+import com.eightstudio.danboru.cinema123.R;
+
+public class splashscreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splashscreen);
+
+       final Thread thread = new Thread(){
+
+           @Override
+           public void run(){
+
+               try{
+
+                   sleep(3000);
+                   Intent jeda = new Intent(getBaseContext(), MainActivity.class);
+                   startActivity(jeda);
+                   finish();
+
+               }catch (Exception e){
+
+                   e.printStackTrace();
+               }
+           }
+       };
+
+        thread.start();
+
+    }
+}
