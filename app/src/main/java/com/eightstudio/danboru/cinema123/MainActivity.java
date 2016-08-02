c802088745b9eac27e813cf193a86405e3bd9af6
Danang <8danang8@gmail.com>
2016-08-02 17:51:43 +0700

    Normal dengan penambahan tab

--- app/src/main/java/com/eightstudio/danboru/cinema123/MainActivity.java
+++ app/src/main/java/com/eightstudio/danboru/cinema123/MainActivity.java
@@ -1,13 +1,16 @@
 package com.eightstudio.danboru.cinema123;
 
 import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 
 
-public class MainActivity extends Activity{
+public class MainActivity extends TabActivity {
 
     Button btnViewProducts;
     Button btnNewProduct;
@@ -17,6 +20,49 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Resources ressources = getResources();
+        TabHost tabHost = getTabHost();
+
+        // Android tab
+        Intent intentAndroid = new Intent().setClass(this, NewProductActivity.class);
+        TabHost.TabSpec tabSpecAndroid = tabHost
+                .newTabSpec("Android")
+                .setIndicator("", ressources.getDrawable(R.drawable.set_icon_1))
+                .setContent(intentAndroid);
+
+        // Apple tab
+        Intent intentApple = new Intent().setClass(this, AllProductsActivity.class);
+        TabHost.TabSpec tabSpecApple = tabHost
+                .newTabSpec("Apple")
+                .setIndicator("", ressources.getDrawable(R.drawable.set_icon_2))
+                .setContent(intentApple);
+
+        // Windows tab
+        Intent intentWindows = new Intent().setClass(this, NewProductActivity.class);
+        TabHost.TabSpec tabSpecWindows = tabHost
+                .newTabSpec("Windows")
+                .setIndicator("", ressources.getDrawable(R.drawable.set_icon_3))
+                .setContent(intentWindows);
+
+        // Blackberry tab
+        Intent intentBerry = new Intent().setClass(this, AllProductsActivity.class);
+        TabHost.TabSpec tabSpecBerry = tabHost
+                .newTabSpec("Berry")
+                .setIndicator("", ressources.getDrawable(R.drawable.set_icon_4))
+                .setContent(intentBerry);
+
+        // add all tabs
+        tabHost.addTab(tabSpecAndroid);
+        tabHost.addTab(tabSpecApple);
+        tabHost.addTab(tabSpecWindows);
+        tabHost.addTab(tabSpecBerry);
+
+        //set Windows tab as default (zero based)
+        tabHost.setCurrentTab(2);
+    }
+
+
+    /*
         // Buttons
         btnViewProducts = (Button) findViewById(R.id.btnViewProducts);
         btnNewProduct = (Button) findViewById(R.id.btnCreateProduct);
@@ -45,4 +91,6 @@ public class MainActivity extends Activity{
             }
         });
     }
+
+    */
 }
